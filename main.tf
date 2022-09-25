89fda099ba4fc8d875f34a9fc248d170fc28cad1
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-09-25 18:31:58 +0200

    Remove provider declaration

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "digitalocean" {
-  token = "${var.do_token}"
-}
-
 resource "digitalocean_domain" "namelivia" {
   name = "${var.domain_name}"
 }
