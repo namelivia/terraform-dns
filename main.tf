c33dcab8b976b8567a4709cfe1ebf99e1fdd70c4
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-09-25 18:39:47 +0200

    Add requirements

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.11"
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
 resource "digitalocean_domain" "namelivia" {
   name = "${var.domain_name}"
 }
